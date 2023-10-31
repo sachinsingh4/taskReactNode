391e3df15ffdaea13ebbf4197a5c019cca77b00f
Your Name <kumarsachin22022@gmail.com>
2023-10-31 10:46:31 +0530

    integrate Map

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Home from "./pages/home/Home";
 import Header from "./components/header/Header";
+import Map from "./pages/map/Map";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Map />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
         </Routes>
---
+++ client/src/pages/map/Map.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
+
+const Map = () => {
+  const containerStyle = {
+    width: "100%",
+    height: "400px",
+  };
+
+  const center = {
+    lat: 23.0522, // Set the initial latitude
+    lng: 72.5497, // Set the initial longitude
+  };
+
+  const [markerPosition, setMarkerPosition] = useState(center);
+  const [address, setAddress] = useState("");
+
+  // Function to handle marker drag end event
+  const onMarkerDragEnd = (event) => {
+    const newLat = event.latLng.lat();
+    const newLng = event.latLng.lng();
+    setMarkerPosition({ lat: newLat, lng: newLng });
+
+    // Perform reverse geocoding to get the address
+    const apiKey = "";
+    const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${newLat},${newLng}&key=${apiKey}`;
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === "OK" && data.results.length > 0) {
+          const formattedAddress = data.results[0].formatted_address;
+          setAddress(formattedAddress);
+        } else {
+          setAddress("Address not found");
+        }
+      })
+      .catch((error) => {
+        setAddress("Error fetching address");
+      });
+
+    console.log(address);
+  };
+
+  return (
+    <LoadScript googleMapsApiKey="AIzaSyBu9pup8uAdJB9EYl1VvMxmPlTb1omxRFE">
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+        {/* Add markers or other map components here */}
+        <MarkerF
+          position={markerPosition}
+          draggable={true}
+          icon={{
+            url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+          }}
+          onDragEnd={onMarkerDragEnd}
+        />
+      </GoogleMap>
+
+      <div>Marker's Address: {address}</div>
+    </LoadScript>
+  );
+};
+
+export default Map;
